474e0f602841ff6eec9ef467e13f5d47082e5808
smccann <61484003+smccann312@users.noreply.github.com>
2020-02-26 02:00:03 -0500

    Update Form.js

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -5,7 +5,7 @@ const form = () => {
     <div>
       <h1>We Are Anonymous</h1>
       <h1>sup - josue</h1>
-      <h2>just testing it out - Shane</h2>
+      <h2>Just testing it out - Shane</h2>
 	  <h3>Thomas check 2</h3>
     </div>
     );
