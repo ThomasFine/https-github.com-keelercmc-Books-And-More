20482c29cf2159475fbc9a97463ae3c00f8e02c2
pardoj1 <60763934+pardoj1@users.noreply.github.com>
2020-02-25 22:21:09 -0500

    Update Form.js

--- src/components/Forms/Form.js
+++ src/components/Forms/Form.js
@@ -4,7 +4,7 @@ const form = () => {
   return (
     <div>
       <h1>We Are Anonymous</h1>
-      <h1>just testing it out</h1>
+      <h1>sup - josue</h1>
       <h2>just testing it out - Shane</h2>
 	  <h3>Thomas check 2</h3>
     </div>
