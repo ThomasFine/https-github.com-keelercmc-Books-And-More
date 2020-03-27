62f81a0e38fe70a78054d9b51bf8cc46615b719c
Chance <keelercmc@gmail.com>
2020-03-27 18:08:46 -0400

    Fix constructor to take props for statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -2,35 +2,37 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-
 class Statistics extends Component {
 
-    state = {
-        totalBooks: 0
-    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: props.username,
+            totalBooks: 0
+        };
+      }
 
     componentDidMount() {
         this.totalBooks();
     }
 
     totalBooks = () => {
+        //Total books read: Sum of entries
         axios.get('https://books-n-more.firebaseio.com/' + 'chance' + '.json').then(response => {
             this.setState({totalBooks: Object.keys(response.data).length});
         });
     }
 
+    //Total pages read: Sum of entry lengths
+    //Average rating: Sum of entry ratings / number of entries
+    //Favorite author: Form array of authors, sort, return most repeated author
+    //Median year: Form array of years, sort, return middle element
+
     render() {
         return(
-            //Total books read: Sum of entries
-            <hi>Total books read: {this.state.totalBooks}</hi>
-
-            //Total pages read: Sum of entry lengths
-
-            //Average rating: Sum of entry ratings / number of entries
-
-            //Favorite author: Form array of authors, sort, return most repeated author
-
-            //Median year: Form array of years, sort, return middle element
+            <div>
+                <hi>Total books read for {this.state.username}: {this.state.totalBooks}</hi>
+            </div>
         );
     }
 } 
