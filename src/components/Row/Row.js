43f5d54f8a5fffcffbec36be7b0d4271e88c1e8d
pardoj1 <60763934+pardoj1@users.noreply.github.com>
2020-03-27 18:39:40 -0400

    Update Row.js

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -4,7 +4,6 @@ import './Row.css';
 const row = (props) => {
     return (
         <div>
-            <input type="checkbox" />
             <h2>{props.title} - </h2>
             <h2>{props.author}</h2>
         </div>
