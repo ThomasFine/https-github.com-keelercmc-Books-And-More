bd6d8037806458d6833f6d64bac4a209e6e7210d
Chance <keelercmc@gmail.com>
2020-03-27 18:02:01 -0400

    Add total books read statistic

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-const statistics = (props) => {
-    return (
-        <hi>{props.username}</hi>
-    );
-}
 
-export default statistics;
+
+class Statistics extends Component {
+
+    state = {
+        totalBooks: 0
+    }
+
+    componentDidMount() {
+        this.totalBooks();
+    }
+
+    totalBooks = () => {
+        axios.get('https://books-n-more.firebaseio.com/' + 'chance' + '.json').then(response => {
+            this.setState({totalBooks: Object.keys(response.data).length});
+        });
+    }
+
+    render() {
+        return(
+            //Total books read: Sum of entries
+            <hi>Total books read: {this.state.totalBooks}</hi>
+
+            //Total pages read: Sum of entry lengths
+
+            //Average rating: Sum of entry ratings / number of entries
+
+            //Favorite author: Form array of authors, sort, return most repeated author
+
+            //Median year: Form array of years, sort, return middle element
+        );
+    }
+} 
+export default Statistics;
