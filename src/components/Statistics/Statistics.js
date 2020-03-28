286fffd0e9fcfcf3e93dd3b545c9821dfe7a61e3
smccann <61484003+smccann312@users.noreply.github.com>
2020-03-28 02:42:20 -0400

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -18,7 +18,7 @@ class Statistics extends Component {
 
     totalBooks = () => {
         //Total books read: Sum of entries
-        axios.get('https://books-n-more.firebaseio.com/' + 'shane' + '.json').then(response => {
+        axios.get('https://books-n-more.firebaseio.com/' + username + '.json').then(response => {
             this.setState({totalBooks: Object.keys(response.data).length});
         });
     }
